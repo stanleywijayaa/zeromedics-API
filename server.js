590435cd1f4c8e04b83a54883111768cd538ec1b
Jinxsuxk <marvinjaylorenz@gmail.com>
2025-08-14 11:47:56 +0800

    product done

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
 require('dotenv').config()
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 // routes
 const customerRoute = require('./routes/customer');
