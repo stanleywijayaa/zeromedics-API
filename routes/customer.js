f1c95f9f87f5598ec1ccdecb4731119dad587f7e
stanleywijayaa <stanleytan2018@gmail.com>
2025-08-14 11:19:26 +0800

    finished backend function to create new customer

--- routes/customer.js
+++ routes/customer.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getCustomer, getAllCustomers } = require('../logics/customerLogic');
+const { createCustomer, getAllCustomers, getCustomer } = require('../logics/customerLogic');
 
+router.post('/', async(req,res) => {
+    const customerData = req.body;
+    try {
+        const newCustomer = await createCustomer(customerData);// need to create html & php to retrieve data from user
+        res.status(201).json(newCustomer);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to create customer' });
+    }
+});
+
+//thinking of retrieving customer data by amount & page bcs only shows 10 customers bcs of pagination
 router.get('/all', async (req, res) => {
     try {
         const customers = await getAllCustomers();
