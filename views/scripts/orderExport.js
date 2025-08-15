7c63c06dcccdc8c4491aed1047c001e9734bcf40
Realxeng <realxengpandelaki@gmail.com>
2025-08-15 12:18:25 +0800

    base of exporting order

--- views/scripts/orderExport.js
+++ views/scripts/orderExport.js
@@ -3,5 +3,5 @@ exportBtn.disabled = true
 
 document.getElementById("exportBtn").addEventListener("click", () => {
     console.log("Exporting orders...");
-    // Add your export logic here (CSV, Excel, PDF, etc.)
+    
 });
